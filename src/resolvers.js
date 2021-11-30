b9c68d883fda6e1af616f1739760fd623acb4c8e
Nikkels <rnikkels@bol.com>
2021-11-30 17:34:16 +0100

    deprecated length field

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,11 +40,14 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSeconds: ({ length }) => length,
   },
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  }
 };
 
 module.exports = resolvers;
